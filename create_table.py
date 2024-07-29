d8d7e88cdfa16da2a6d0b4c9c97067bb50234d91
Алексей Гусаков <alongenet32@gmail.com>
2024-07-29 18:17:00 +0300

    fix bug with database created

--- create_table.py
+++ create_table.py
@@ -2,7 +2,7 @@ from sqlalchemy import  text
 
 def create_db(engine, numb_x, numb_y):
 	with engine.begin() as conn:
-		conn.execute(text("CREATE TABLE some_table(x int, y int)"))
+		conn.execute(text("CREATE TABLE IF NOT EXISTS some_table(x int, y int)"))
 		conn.execute(
 			text("INSERT INTO some_table (x, y) VALUES(:x, :y)"),
 			[{"x": numb_x, "y": numb_y}]
