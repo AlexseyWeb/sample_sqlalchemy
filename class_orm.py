5975883d15737254ce923f87a48bb018ead4b8fb
Алексей Гусаков <alongenet32@gmail.com>
2024-08-08 11:40:58 +0300

    add selection

--- class_orm.py
+++ class_orm.py
@@ -23,17 +23,25 @@ Base.metadata.create_all(bind=engine)
 
 Session = sessionmaker(autoflush=False, bind=engine)
 
+# with Session(autoflush=False, bind=engine) as db:
+# 	tom = Person(name="Tom", age=32)
+# 	jack = Person(name="Jack", age=28)
+# 	alex = Person(name="Alex", age=23)
+# 	db.add(tom)
+# 	db.add(jack)
+# 	db.add(alex)
+# 	db.commit()
+# 	print(tom.id)
+print("\t\t\tid name age")
 with Session(autoflush=False, bind=engine) as db:
-	tom = Person(name="Tom", age=32)
-	jack = Person(name="Jack", age=28)
-	alex = Person(name="Alex", age=23)
-	db.add(tom)
-	db.add(jack)
-	db.add(alex)
-	db.commit()
-	print(tom.id)
-
-with Session(autoflush=False, bind=engine):
 	people = db.query(Person).all()
 	for i in people:
-		print(f"{i.id}.{i.name} {i.age}")
+		print(f" Получение данных всех {i.id}.{i.name} {i.age}")
+
+with Session(autoflush=False, bind=engine) as db:
+	first_person = db.get(Person, 1)
+	print(f" Получение по id через Get {first_person.name} - {first_person.age}")
+
+with Session(autoflush=False, bind=engine):
+	people = db.query(Person).filter(Person.age > 30).first()
+	print(f" Фильтрация запроса id = {i.id} name = {i.name} age = {i.age}")
